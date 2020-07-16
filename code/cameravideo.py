250602b8b675c9c8c8ed1acda90ffeb2f763c776
HeJayce <hejayce@gmail.com>
2020-07-16 12:25:59 +0800

    7.16am

---
+++ code/cameravideo.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from PyQt5.QtGui import QPixmap,QImage
+
+
+
+
+'''
+摄像头操作：创建类对象完成摄像头的操作，把打开摄像头与创建类对象操作合并
+    __init__函数完成摄像头的配置打开
+'''
+
+class camera():
+    def __init__(self):
+        #创建摄像头。对视频或摄像头进行读取操作
+        #参数fliename 、device（视频设备号，0默认，123多个）
+        self.capture = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        self.currentframe = np.array([])       #创建多维数组
+
+
+    def read_camera(self):
+        ret,data = self.capture.read()
+        if not ret :
+            print("获取摄像头失败")
+            return None
+        return data 
+
+    def camera_to_pic(self):
+        pic = self.read_camera()
+        #摄像头是BGR方式在站，首先需要转换为RGB
+        #调icvtColor完成后才是RGB悠式的画面数据
+        self.currentframe = cv2.cvtColor(pic,cv2.COLOR_BGR2RGB)
+        #设置宽高
+        #self.currentframe = cv2.cvtColor(self.currentframe,(640,480))
+
+        #转换格式（界面能够显示的格式）
+        #获取画面的宽度和高度
+        height,width = self.currentframe.shape[:2]
+        #先转换成QImage类型的图片（画面）,创建Qimage类对象，使用摄像头的画面数据
+        #QIMAGE（data，width，height，format）创建：数据，宽度，高度
+        qimg = QImage(self.currentframe,width,height,QImage.Format_RGB888)
+        qpixmap = QPixmap.fromImage(qimg)
+        return qpixmap
--- code/mywindow.py
+++ code/mywindow.py
@@ -1,7 +1,8 @@
-
 from Ui_Mainwindows import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow,QApplication
 import sys
+from PyQt5.QtCore import QTimer
+from cameravideo import camera
 '''
 子类，维承UI_MainWindow与QMainWindow
 Ui_ MainWindow:
@@ -10,13 +11,55 @@ QMainWindow:
     包含是整个界面窗口，窗口的操作
 mywindow:
     完整的窗口类
-
-
 '''
 class mywindow(Ui_MainWindow,QMainWindow):
     def __init__(self):         #初始化函数
         super(mywindow,self).__init__()
         self.setupUi(self)   #创建界面内容
+        #信号与槽的关联
+        #self.actionopen：指定对象
+        #triggered 信号
+        #connect ：关联（槽函数）
+        #self.on_actionopen  关联的函数
+        self.actionopen.triggered.connect(self.on_actionopen)
+        self.actionopen.triggered.connect(self.on_actionclose)
+    '''
+    信号槽功能：
+    当某个组件设计了信号槽功能时，当信号产生，会主动调用槽函数，去完成对应的
+    信号：当以某种特定的操作，操作这个组件时，就会主动产生对应操作的信号
+    '''
+
+    def on_actionopen(self):
+        #print("on_actionopen")
+        #启动摄像头
+        self.cameravideo = camera() 
+        #启动定时器，进行定时，每隔多久进行一次获取摄像头数据进行显示
+        self.timeshow = QTimer(self)
+        self.timeshow.start(50)
+        #50ms  定时器启动，每50ms就会产生一个信号timeout
+        self.timeshow.timeout.connect(self.show_cameradata)
+
+    def on_actionclose(self):
+        print("on_actionclose")
+
+
+
+
+
+
+    def show_cameradata(self):
+        #获取摄像头数据，转换数据
+        pic = self.cameravideo.camera_to_pic()
+        #显示数据，显示画面
+        self.label_6.setPixmap(pic)
+
+
+
+
+
+
+
+
 
 #创建应用程序对象
 app = QApplication(sys.argv)
